b678262b5ebc4accebd75e84c191f6fc0315bc1d
Rick Pannekoek <rpannekoek@sdl.com>
2016-06-01 21:10:36 +0200

    TSI-789 Now including Context Expressions ExtensionData in DD4T JSON (TSI-1581)

---
+++ Sdl.Web.Tridion.Templates/Templates/DD4T/ContextExpressionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Tridion.ContentManager.AudienceManagement;
+
+namespace Sdl.Web.Tridion.Templates.DD4T
+{
+    internal static class ContextExpressionManager
+    {
+        private static readonly Regex _titleRegex = new Regex(@"^[\p{L}_][\p{L}\p{N}_]*\.[\p{L}_][\p{L}\p{N}_]*$", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        internal static bool HasContextExpression(TargetGroup targetGroup)
+        {
+            // TODO: Also check for CE App Data (?)
+            return _titleRegex.IsMatch(targetGroup.Title);
+        }
+
+        internal static string[] GetContextExpressions(IEnumerable<TargetGroup> targetGroups)
+        {
+            return targetGroups.Where(HasContextExpression).Select(tg => tg.Title).ToArray();
+        }
+    }
+}
