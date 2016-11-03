9a3d975cf65f91eb173bd5c078203ed7dddcd818
Rick Pannekoek <rpannekoek@sdl.com>
2016-11-03 15:44:08 +0100

    TSI-811 Adding Keyword MetadataSchemaId as DXA Extension Data (TSI-2101) Also made DD4T link level configurable as TBB parameter (also for "Generate Dynamic Page (DXA)").

--- Sdl.Web.Tridion.Templates/Templates/DD4T/DxaBuildManager.cs
+++ Sdl.Web.Tridion.Templates/Templates/DD4T/DxaBuildManager.cs
@@ -1,14 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using DD4T.ContentModel;
 using DD4T.Templates.Base.Builder;
+using Tridion.ContentManager.ContentManagement.Fields;
 using Tridion.ContentManager.Templating;
 using Dd4tComponentPresentation = DD4T.ContentModel.ComponentPresentation;
 using TcmComponentPresentation = Tridion.ContentManager.CommunicationManagement.ComponentPresentation;
+using TcmKeyword = Tridion.ContentManager.ContentManagement.Keyword;
 
 namespace Sdl.Web.Tridion.Templates.DD4T
 {
     internal class DxaBuildManager : BuildManager
     {
         private const string ContextExpressionSectionName = "ContextExpressions";
+        private const string DxaExtensionDataSectionName = "DXA";
 
         private readonly TemplatingLogger _logger = TemplatingLogger.GetLogger(typeof(DxaBuildManager));
 
@@ -18,20 +23,50 @@ namespace Sdl.Web.Tridion.Templates.DD4T
             // Minimize the amount of unneeded data in the DD4T JSON:
             BuildProperties.OmitFolders = true;
             BuildProperties.OmitCategories = true;
-            BuildProperties.OmitCategories = true;
             BuildProperties.OmitContextPublications = true;
             BuildProperties.OmitOwningPublications = true;
+            BuildProperties.OmitValueLists = true;
+
+            BuildProperties.ECLEnabled = true; // ECL is only used for ECL Stubs
+
+            string expandLinkDepth = package.GetValue("expandLinkDepth");
+            _logger.Debug(string.Format("expandLinkDepth: {0}", expandLinkDepth));
+            if (!string.IsNullOrEmpty(expandLinkDepth))
+            {
+                BuildProperties.LinkLevels = Convert.ToInt32(expandLinkDepth);
+            }
         }
 
         public override Dd4tComponentPresentation BuildComponentPresentation(TcmComponentPresentation tcmComponentPresentation, Engine engine,
             int linkLevels, bool resolveWidthAndHeight)
         {
             Dd4tComponentPresentation result = base.BuildComponentPresentation(tcmComponentPresentation, engine, linkLevels, resolveWidthAndHeight);
-            AddExtensionData(result, tcmComponentPresentation);
+            AddCpExtensionData(result, tcmComponentPresentation);
             return result;
         }
 
-        private void AddExtensionData(Dd4tComponentPresentation dd4tComponentPresentation, TcmComponentPresentation tcmComponentPresentation)
+        public override Field BuildField(ItemField tcmItemField, int currentLinkLevel)
+        {
+            Field dd4tField = base.BuildField(tcmItemField, currentLinkLevel);
+
+            KeywordField tcmKeywordField = tcmItemField as KeywordField;
+            if (tcmKeywordField != null)
+            {
+                int i = 0;
+                foreach (Keyword dd4tKeyword in dd4tField.KeywordValues)
+                {
+                    TcmKeyword tcmKeyword = tcmKeywordField.Values[i++];
+                    if (tcmKeyword.MetadataSchema != null)
+                    {
+                        dd4tKeyword.AddExtensionProperty(DxaExtensionDataSectionName, "MetadataSchemaId", tcmKeyword.MetadataSchema.Id);
+                    }
+                }
+            }
+
+            return dd4tField;
+        }
+
+        private void AddCpExtensionData(Dd4tComponentPresentation dd4tComponentPresentation, TcmComponentPresentation tcmComponentPresentation)
         {
             if (!tcmComponentPresentation.Conditions.Any())
             {
---
+++ Sdl.Web.Tridion.Templates/Templates/DD4T/GenerateDynamicComponent.cs
@@ -0,0 +1,41 @@
+﻿using DD4T.Templates.Base;
+using Tridion.ContentManager.Templating;
+using Tridion.ContentManager.Templating.Assembly;
+using Dynamic = DD4T.ContentModel;
+
+
+namespace Sdl.Web.Tridion.Templates.DD4T
+{
+    /// <summary>
+    /// Generates a DD4T data model based on the current component
+    /// </summary>
+    [TcmTemplateTitle("Generate Dynamic Component (DXA)")]
+    [TcmTemplateParameterSchema("resource:Sdl.Web.Tridion.Resources.GenerateDynamicComponentParameters.xsd")]
+    public class GenerateDynamicComponent : BaseComponentTemplate
+    {
+
+        public GenerateDynamicComponent()
+            : base(TemplatingLogger.GetLogger(typeof (GenerateDynamicComponent)))
+        {
+            
+        }
+
+        /// <summary>
+        /// Performs the Transform. Overridden here so we can inject our <see cref="DxaBuildManager"/>.
+        /// </summary>
+        /// <param name="engine"></param>
+        /// <param name="package"></param>
+        public override void Transform(Engine engine, Package package)
+        {
+            Log.Debug("Transform");
+            Manager = new DxaBuildManager(package, engine);
+            base.Transform(engine, package);
+        }
+
+        #region DynamicDeliveryTransformer Members
+        protected override void TransformComponent(Dynamic.Component component)
+        {
+        }
+        #endregion
+    }
+}
