14e6a95c6c7d5fcfa62795763719a3121d60ab5d
Mykhailo Sapiegin <msapiegin@sdl.com>
2017-08-31 17:25:34 +0300

    Unit tests refactored. TestEngine initialization extended with mocking PublishingContext. Override for RunTemplate() added.

--- Sdl.Web.Tridion.Templates.Tests/PublishMappingsTest.cs
+++ Sdl.Web.Tridion.Templates.Tests/PublishMappingsTest.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Sdl.Web.DataModel.Configuration;
+using Newtonsoft.Json;
 using Tridion.ContentManager.CommunicationManagement;
 using Tridion.ContentManager.ContentManagement;
 using Tridion.ContentManager.Publishing.Rendering;
@@ -11,6 +12,8 @@ namespace Sdl.Web.Tridion.Templates.Tests
     [TestClass]
     public class PublishMappingsTest : TemplateTest
     {
+        private static readonly string CoreCoponentWebDavUrl = "/webdav/100 Master/Building Blocks/Modules/Core/Admin/Core.xml";
+        private static readonly string RegionJsonFile = "/Preview/system/mappings/regions.json";
 
         [ClassInitialize]
         public static void Initialize(TestContext testContext)
@@ -22,23 +25,15 @@ namespace Sdl.Web.Tridion.Templates.Tests
         public void AddNativeRegionNotUniqueName_Success()
         {
             const string regionShemaTitle = "Main"; // Region with this name already exists in DXA Templates 
-            const string nestedRegionSchemaTitle = "AddNativeRegionUniqueNameSuccess_R";
+            const string nestedRegionSchemaTitle = "AddNativeRegionNotUniqueName_Success";
             Schema regionSchema = null;
             Schema nestedRegionSchema = null;
+             
             try
             {
                 //init engine
-                Component inputItem = (Component)TestSession.GetObject("/webdav/400 Example Site/Building Blocks/Content/Sitemap.xml");
-                RenderedItem testRenderedItem = CreateTestRenderedItem(inputItem, null);
-                var testEngine = new TestEngine(testRenderedItem);
-
-                //init package
-                var testPackage = new Package(testEngine);
-                Type inputItemType = inputItem.GetType();
-                string inputItemName = inputItemType.Name;
-                ContentType inputItemContentType = new ContentType($"tridion/{inputItemType.Name.ToLower()}");
-                testPackage.PushItem(inputItemName, testPackage.CreateTridionItem(inputItemContentType, inputItem));
-
+                Component inputItem = (Component)TestSession.GetObject(CoreCoponentWebDavUrl);
+               
                 // Create TestData Regions
                 Publication testPublication = (Publication)inputItem.ContextRepository;
 
@@ -59,8 +54,14 @@ namespace Sdl.Web.Tridion.Templates.Tests
                 };
                 regionSchema.Save(true);
 
-                SummaryData[] sitemapRoot = RunTemplate<SummaryData[]>(typeof(PublishMappings), inputItem);
-                //TODO: Assertion on Regions.JSON
+                RenderedItem testRenderedItem;
+                Package testPackage = RunTemplate(typeof(PublishMappings), inputItem, out testRenderedItem);
+                Item testItem = testPackage.GetByName("/Preview/system/mappings/regions.json");
+                string content = testItem.GetAsString();
+                var definedRegions = JsonConvert.DeserializeObject<List<RegionDefinitionTest>>(content);
+                Assert.IsTrue(definedRegions.Count(x => x.Region == regionShemaTitle && x.ComponentTypes.Any()) == 1,
+                    "DXA 'Main' Region and its ComponentTypes not overridden by TCM Region");
+
             }
             finally
             {
@@ -75,25 +76,16 @@ namespace Sdl.Web.Tridion.Templates.Tests
         [TestMethod]
         public void AddNativeRegionUniqueName_Success()
         {
-            const string regionShemaTitle = "AddNativeRegionUniqueNameSuccess_R";
-            const string nestedRegionSchemaTitle = "AddNativeRegionUniqueNameSuccess_N";
+            const string regionShemaTitle = "AddNativeRegionUniqueName_Success1";
+            const string nestedRegionSchemaTitle = "AddNativeRegionUniqueName_Success2";
             Schema regionSchema = null;
             Schema nestedRegionSchema = null;
-
+            
             try
             {
                 //init engine
-                Component inputItem = (Component)TestSession.GetObject("/webdav/400 Example Site/Building Blocks/Content/Sitemap.xml");
-                RenderedItem testRenderedItem = CreateTestRenderedItem(inputItem, null);
-                var testEngine = new TestEngine(testRenderedItem);
-
-                //init package
-                var testPackage = new Package(testEngine);
-                Type inputItemType = inputItem.GetType();
-                string inputItemName = inputItemType.Name;
-                ContentType inputItemContentType = new ContentType($"tridion/{inputItemType.Name.ToLower()}");
-                testPackage.PushItem(inputItemName, testPackage.CreateTridionItem(inputItemContentType, inputItem));
-
+                Component inputItem = (Component)TestSession.GetObject(CoreCoponentWebDavUrl);
+               
                 // Create TestData Regions
                 Publication testPublication = (Publication)inputItem.ContextRepository;
 
@@ -114,8 +106,14 @@ namespace Sdl.Web.Tridion.Templates.Tests
                 };
                 regionSchema.Save(true);
 
-                SummaryData[] sitemapRoot = RunTemplate<SummaryData[]>(typeof(PublishMappings), inputItem);
-                //TODO: Assertion on Regions.JSON
+                RenderedItem testRenderedItem;
+                Package testPackage = RunTemplate(typeof(PublishMappings), inputItem, out testRenderedItem);
+                Item testItem = testPackage.GetByName("/Preview/system/mappings/regions.json");
+                var content = testItem.GetAsString();
+                var definedRegions = JsonConvert.DeserializeObject<List<RegionDefinitionTest>>(content);
+                Assert.IsTrue(definedRegions.Count(x => x.Region==regionShemaTitle && !x.ComponentTypes.Any()) == 1, 
+                    "TCM Region without any Component Presentations added to Regions.json");
+
             }
             finally
             {
@@ -130,22 +128,14 @@ namespace Sdl.Web.Tridion.Templates.Tests
         [TestMethod]
         public void AddNoNativeRegions_Success()
         {
-            const string regionShemaTitle = "RegionSchema";
+            const string regionShemaTitle = "AddNoNativeRegions_Success";
             Schema regionSchema = null;
+            
             try
             {
                 //init engine
-                Component inputItem = (Component)TestSession.GetObject("/webdav/400 Example Site/Building Blocks/Content/Sitemap.xml");
-                RenderedItem testRenderedItem = CreateTestRenderedItem(inputItem, null);
-                var testEngine = new TestEngine(testRenderedItem);
-
-                //init package
-                var testPackage = new Package(testEngine);
-                Type inputItemType = inputItem.GetType();
-                string inputItemName = inputItemType.Name;
-                ContentType inputItemContentType = new ContentType($"tridion/{inputItemType.Name.ToLower()}");
-                testPackage.PushItem(inputItemName, testPackage.CreateTridionItem(inputItemContentType, inputItem));
-
+                Component inputItem = (Component)TestSession.GetObject(CoreCoponentWebDavUrl);
+               
                 // Create TestData Regions
                 Publication testPublication = (Publication)inputItem.ContextRepository;
 
@@ -157,41 +147,38 @@ namespace Sdl.Web.Tridion.Templates.Tests
                 };
                 regionSchema.Save(true);
 
-                SummaryData[] sitemapRoot = RunTemplate<SummaryData[]>(typeof(PublishMappings), inputItem);
-                //TODO: Assertion on Regions.JSON
+                RenderedItem testRenderedItem;
+                Package testPackage = RunTemplate(typeof(PublishMappings), inputItem, out testRenderedItem);
+                Item testItem = testPackage.GetByName("/Preview/system/mappings/regions.json");
+                string content = testItem.GetAsString();
+                var definedRegions = JsonConvert.DeserializeObject<List<RegionDefinitionTest>>(content);
+                Assert.IsFalse(definedRegions.Any(x => x.Region == regionShemaTitle),
+                    "No TCM Region added to Regions.json");
             }
             finally
             {
                 //Cleanup
-                regionSchema?.Delete();
+               regionSchema?.Delete();
             }
         }
 
         [Ignore]
         [Description("Ignore until DXA unit tests use at least 8.7 TCM version")]
         [TestMethod]
-        public void AddNestedRegionSchemas_Success()
+        public void AddNotUniqueRegions_Success()
         {
-            const string regionShemaTitle = "Main"; // Region with this name already exists in DXA Templates 
-            const string nestedRegionSchemaTitle = "AddNativeRegionUniqueNameSuccess_R";
-            const string superNestedRegionSchemaTitle = "superNestedRegionSchemaTitle";
+            const string regionShemaTitle = "AddNotUniqueRegions_Success1"; // Region with this name already exists in DXA Templates 
+            const string nestedRegionSchemaTitle = "AddNotUniqueRegions_Success2";
+            const string superNestedRegionSchemaTitle = "AddNotUniqueRegions_Success3";
             Schema regionSchema = null;
             Schema nestedRegionSchema = null;
             Schema superNestedRegionSchema = null;
+           
             try
             {
                 //init engine
-                Component inputItem = (Component)TestSession.GetObject("/webdav/400 Example Site/Building Blocks/Content/Sitemap.xml");
-                RenderedItem testRenderedItem = CreateTestRenderedItem(inputItem, null);
-                var testEngine = new TestEngine(testRenderedItem);
-
-                //init package
-                var testPackage = new Package(testEngine);
-                Type inputItemType = inputItem.GetType();
-                string inputItemName = inputItemType.Name;
-                ContentType inputItemContentType = new ContentType($"tridion/{inputItemType.Name.ToLower()}");
-                testPackage.PushItem(inputItemName, testPackage.CreateTridionItem(inputItemContentType, inputItem));
-
+                Component inputItem = (Component)TestSession.GetObject(CoreCoponentWebDavUrl);
+                
                 // Create TestData Regions
                 Publication testPublication = (Publication)inputItem.ContextRepository;
 
@@ -217,12 +204,18 @@ namespace Sdl.Web.Tridion.Templates.Tests
                     Purpose = SchemaPurpose.Region,
                     Title = regionShemaTitle,
                     Description = regionShemaTitle,
-                    RegionDefinition = { NestedRegions = { { regionShemaTitle, nestedRegionSchema } } }
+                    RegionDefinition = { NestedRegions = { { nestedRegionSchemaTitle, nestedRegionSchema } } }
                 };
                 regionSchema.Save(true);
 
-                SummaryData[] sitemapRoot = RunTemplate<SummaryData[]>(typeof(PublishMappings), inputItem);
-                //TODO: Assertion on Regions.JSON
+                RenderedItem testRenderedItem;
+                Package testPackage = RunTemplate(typeof(PublishMappings), inputItem, out testRenderedItem);
+                Item testItem =  testPackage.GetByName(RegionJsonFile);
+                string content = testItem.GetAsString();
+                var definedRegions = JsonConvert.DeserializeObject<List<RegionDefinitionTest>>(content);
+                Assert.IsTrue(definedRegions.Count(x => x.Region == regionShemaTitle) == 1,
+                    "Despite Different Schemas has Regions with the same name only one of those added to Regions.json");
+
             }
             finally
             {
@@ -231,6 +224,21 @@ namespace Sdl.Web.Tridion.Templates.Tests
                 nestedRegionSchema?.Delete();
                 superNestedRegionSchema?.Delete();
             }
+        }
+        private class RegionDefinitionTest
+        {
+            [JsonProperty("Region")]
+            public string Region { get; set; }
+            [JsonProperty("ComponentTypes")]
+            public List<ComponentTypesTest> ComponentTypes { get; set; }
+        }
+
+        private class ComponentTypesTest
+        {
+            [JsonProperty("Schema")]
+            public string Schema { get; set; }
+            [JsonProperty("Template")]
+            public string Template { get; set; }
         }
     }
 }
