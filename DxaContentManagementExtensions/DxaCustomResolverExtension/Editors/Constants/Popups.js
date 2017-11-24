356c6f5c679675ed69427e5f12586f8178b3978e
mborysenko <mborysenko@sdl.com>
2017-11-24 17:27:16 +0200

    TSI-2886 > Reduced size of a popup window

--- DxaContentManagementExtensions/DxaCustomResolverExtension/Editors/Constants/Popups.js
+++ DxaContentManagementExtensions/DxaCustomResolverExtension/Editors/Constants/Popups.js
@@ -2,5 +2,5 @@ Tridion.Type.registerNamespace("Tridion.Web.UI.Editors.CME.Constants.Popups");
 
 Tridion.Web.UI.Editors.CME.Constants.Popups.CUSTOM_RESOLVER_SETTINGS = {
     URL: $config.expandEditorPath("/Views/Popups/Settings/Settings.aspx", "CustomResolver"),
-    FEATURES: "width=720px,height=480px,resizable=1"
+    FEATURES: "width=480px,height=360px,resizable=1"
 };
