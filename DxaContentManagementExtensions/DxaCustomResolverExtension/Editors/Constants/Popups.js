881188f8c9308240daf4542a00115ed4afe6c39c
mborysenko <mborysenko@sdl.com>
2017-11-24 14:20:50 +0200

    TSI-2886 > Adjusted DXA Custom Resolver Settings dialog window size

--- DxaContentManagementExtensions/DxaCustomResolverExtension/Editors/Constants/Popups.js
+++ DxaContentManagementExtensions/DxaCustomResolverExtension/Editors/Constants/Popups.js
@@ -2,5 +2,5 @@ Tridion.Type.registerNamespace("Tridion.Web.UI.Editors.CME.Constants.Popups");
 
 Tridion.Web.UI.Editors.CME.Constants.Popups.CUSTOM_RESOLVER_SETTINGS = {
     URL: $config.expandEditorPath("/Views/Popups/Settings/Settings.aspx", "CustomResolver"),
-    FEATURES: "resizable=1"
+    FEATURES: "width=720px,height=480px,resizable=1"
 };
