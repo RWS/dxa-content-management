720995b161c0dbac8656874e1071374170c08d68
Paul Adams <majiccode@gmail.com>
2017-12-06 16:15:55 +0000

    TSI-2934 : refactor to remove recursion from resolver to avoid stack overflows when using inifite recursion depth + >1000 level hierarchy

--- Sdl.Web.DXAResolver/Resolver.cs
+++ Sdl.Web.DXAResolver/Resolver.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Xml.Linq;
 using Tridion.ContentManager;
 using Tridion.ContentManager.CommunicationManagement;
@@ -13,7 +15,7 @@ using Tridion.ContentManager.Publishing.Resolving;
 using Tridion.ContentManager.Templating;
 
 namespace Sdl.Web.DXAResolver
-{  
+{   
     /// <summary>
     /// DXA Custom Resolver to publish components added to a page with the 'Generate Data Presentation' template.
     /// 
@@ -36,7 +38,7 @@ namespace Sdl.Web.DXAResolver
     {
         private int _maxRecurseDepth = -1; // default = infinite recursion depth
         private readonly TemplatingLogger _log;
-       
+
         public Resolver()
         {
             // init logging
@@ -44,129 +46,137 @@ namespace Sdl.Web.DXAResolver
             _log.Debug($"DXA Custom Resolver initialised with recurse depth {_maxRecurseDepth}");            
         }
 
-        private List<ResolvedItem> Resolve(ItemField itemField, ComponentTemplate template,
-            HashSet<IdentifiableObject> resolved, int recurseLevel)
+        private List<Component> GatherLinkedComponents(Component component)
         {
-            List<ResolvedItem> toResolve = new List<ResolvedItem>();
-            if (itemField == null) return toResolve;
-            if (itemField is ComponentLinkField)
+            _log.Debug($"Gathering linked components for component {component.Title}");
+            List<ItemFields> fieldList = new List<ItemFields>();
+            List<Component> components = new List<Component>();
+            if (component.Content != null)
             {
-                ComponentLinkField linkField = (ComponentLinkField) itemField;
+                fieldList.Add(new ItemFields(component.Content, component.Schema));
+            }
+            if (component.Metadata != null)
+            {
+                fieldList.Add(new ItemFields(component.Metadata, component.MetadataSchema));
+            }            
+            var componentLinkFields = new List<ComponentLinkField>();
+            var embeddedSchemaFields = new List<EmbeddedSchemaField>();
+            foreach (var fields in fieldList)
+            {
+                foreach (var field in fields)
+                {
+                    if (field is ComponentLinkField)
+                    {
+                        componentLinkFields.Add((ComponentLinkField)field);
+                    }
+
+                    if (field is EmbeddedSchemaField)
+                    {
+                        embeddedSchemaFields.Add((EmbeddedSchemaField) field);
+                    }
+                }
+            }
+
+            for (int i = 0; i < embeddedSchemaFields.Count; i++)
+            {
+                EmbeddedSchemaField linkField = embeddedSchemaFields[i];
+                EmbeddedSchemaFieldDefinition linksFieldDefinition = linkField.Definition as EmbeddedSchemaFieldDefinition;
+                if (linksFieldDefinition?.EmbeddedSchema == null) continue;
+                TcmUri id = linksFieldDefinition.EmbeddedSchema.Id; // force schema load
+                foreach (var embeddedFields in linkField.Values)
+                {
+                    foreach (var embeddedField in embeddedFields)
+                    {
+                        if (embeddedField is ComponentLinkField)
+                        {
+                            componentLinkFields.Add((ComponentLinkField)embeddedField);
+                        }
+                        if (embeddedField is EmbeddedSchemaField)
+                        {
+                            embeddedSchemaFields.Add((EmbeddedSchemaField)embeddedField);
+                        }
+                    }
+                }
+            }
+
+            foreach (var linkField in componentLinkFields)
+            {
                 if (linkField.Values != null)
                 {
-                    foreach (var linkedComponent in linkField.Values)
-                    {
-                        toResolve.AddRange(Resolve(linkedComponent, template, resolved, recurseLevel + 1));
-                    }
+                    components.AddRange(linkField.Values);
                 }
             }
-
-            if (itemField is EmbeddedSchemaField)
-            {
-                EmbeddedSchemaField linkField = (EmbeddedSchemaField)itemField;
-                EmbeddedSchemaFieldDefinition linksFieldDefinition =
-                               linkField.Definition as EmbeddedSchemaFieldDefinition;
-
-                if (linksFieldDefinition?.EmbeddedSchema != null)
-                {
-                    TcmUri id = linksFieldDefinition.EmbeddedSchema.Id; // force schema load
-                    foreach (ItemFields embeddedFields in linkField.Values)
-                    {
-                        toResolve.AddRange(Resolve(embeddedFields, template, resolved, recurseLevel));
-                    }
-                }
-            }
-
-            return toResolve;
+            return components;
         }
+      
+        private bool ContinueRecursion(int depth) => _maxRecurseDepth < 0 || depth <= _maxRecurseDepth;
 
-        private List<ResolvedItem> Resolve(ItemFields itemFields, ComponentTemplate template,
-            HashSet<IdentifiableObject> resolved, int recurseLevel)
+        private ResolvedItem ResolveComponent(Component component, ComponentTemplate template, HashSet<IdentifiableObject> resolved, int recurseLevel)
         {
-            List<ResolvedItem> toResolve = new List<ResolvedItem>();
-            foreach (var field in itemFields)
-            {
-                toResolve.AddRange(Resolve(field, template, resolved, recurseLevel));
-            }
-            return toResolve;
-        }
-
-        private List<ResolvedItem> Resolve(Component component, ComponentTemplate template, HashSet<IdentifiableObject> resolved, int recurseLevel)
-        {
-            _log.Debug($"Attempting to resolve component: title='{component.Title}' id='{component.Id}' recurseLevel={recurseLevel} maxRecurseDepth={_maxRecurseDepth}");
-            List<ResolvedItem> toResolve = new List<ResolvedItem>();
-            if (_maxRecurseDepth >= 0 && recurseLevel > _maxRecurseDepth)
+            _log.Debug($"Attempting to resolve component: title='{component.Title}' id='{component.Id}'");
+            if (!ContinueRecursion(recurseLevel))
             {
                 _log.Debug($"Reached max recusion level when trying to resolve component {component.Id}, skipping.");
-                return toResolve;
+                return null;
             }
             if (resolved.Contains(component))
             {
                 _log.Debug($"Already resolved component {component.Id}, skipping.");
-                return toResolve;
-            }
-            Stopwatch t = new Stopwatch();
-            t.Start();
+                return null;
+            }            
             TcmUri schemaId = component.Schema.Id; // force load of schema!
             if (template.RelatedSchemas.Contains(component.Schema))
             {
-                toResolve.Add(new ResolvedItem(component, template));
-                resolved.Add(component);
-            }          
-            if (component.Content != null)
-            {
-                ItemFields fields = new ItemFields(component.Content, component.Schema);
-                toResolve.AddRange(Resolve(fields, template, resolved, recurseLevel));
+                return new ResolvedItem(component, template);
             }
-            if (component.Metadata != null)
-            {
-                ItemFields fields = new ItemFields(component.Metadata, component.MetadataSchema);
-                toResolve.AddRange(Resolve(fields, template, resolved, recurseLevel));
-            }
-            t.Stop();
-            _log.Debug($"Resolving component {component.Id} took {t.ElapsedMilliseconds}ms to complete and resulted in {toResolve.Count} items being resolved.");
-            return toResolve;            
-        }
-
-        private List<ResolvedItem> Resolve(Page page, ComponentTemplate template, HashSet<IdentifiableObject> resolved, int recurseLevel)
-        {
-            _log.Debug($"Attempting to resolve component presentation(s) on page: title='{page.Title}' id='{page.Id}' recurseLevel={recurseLevel} maxRecurseDepth={_maxRecurseDepth}");
-            List<ResolvedItem> toResolve = new List<ResolvedItem>();
-            if (resolved.Contains(page))
-            {
-                _log.Debug($"Already resolved page {page.Id}, skipping.");
-                return toResolve;
-            }
-            if (page.ComponentPresentations.Count <= 0)
-            {
-                _log.Debug($"No component presentations on page {page.Id}, skipping.");
-                return toResolve;
-            }
-            if (_maxRecurseDepth >= 0 && recurseLevel > _maxRecurseDepth)
-            {
-                _log.Debug($"Reached max recusion level when trying to resolve page {page.Id}, skipping.");
-                return toResolve;
-            }
-            Stopwatch t = new Stopwatch();
-            t.Start();
-            foreach (var cp in page.ComponentPresentations)
-            {
-                toResolve.AddRange(Resolve(cp.Component, template, resolved, 
-                    recurseLevel == 0 ? recurseLevel : recurseLevel + 1));
-            }
-            t.Stop();
-            _log.Debug($"Resolving page {page.Id} took {t.ElapsedMilliseconds}ms to complete and resulted in {toResolve.Count} items being resolved.");
-            return toResolve;
-        }
-
-        private List<ResolvedItem> Resolve(IdentifiableObject item,
+            return null;
+        }      
+       
+        private List<ResolvedItem> ResolveItem(IdentifiableObject item,
             ComponentTemplate template, HashSet<IdentifiableObject> resolved, int recurseLevel)
         {
             List<ResolvedItem> toResolve = new List<ResolvedItem>();
-            if (item is Component)
-                toResolve.AddRange(Resolve((Component) item, template, resolved, recurseLevel));
+            if (!ContinueRecursion(recurseLevel))
+            {
+                _log.Debug($"Reached max recusion level when trying to resolve item {item.Id}, skipping.");
+                return toResolve;
+            }
+            List<Component> components = new List<Component>();
             if (item is Page)
-                toResolve.AddRange(Resolve((Page) item, template, resolved, recurseLevel));
+            {
+                var page = (Page) item;
+                if (resolved.Contains(page))
+                {
+                    return toResolve;
+                }
+                components.AddRange(page.ComponentPresentations.Select(cp => cp.Component));
+            }
+            if (item is Component)
+            {
+                components.Add(item as Component);
+            }
+            if (components.Count <= 0) return toResolve;
+            var toProcess = new HashSet<Component>();
+            var depths = new Dictionary<Component, int>();
+            for (int i = 0; i < components.Count; i++)
+            { // note: avoiding recursive approach here so we can truely do infinite depth without stack overflows
+                var c = components[i];
+                int depth = recurseLevel;
+                if (depths.ContainsKey(c)) depth = depths[c];
+                if (!ContinueRecursion(depth)) continue;
+                if (toProcess.Contains(c)) continue;
+                toProcess.Add(c);
+                List<Component> linked = GatherLinkedComponents(c);
+                foreach (var linkedComponent in linked)
+                {
+                    if (!ContinueRecursion(depth + 1)) break;
+                    if (toProcess.Contains(linkedComponent)) continue;
+                    components.Add(linkedComponent);
+                    depths.Add(linkedComponent, depth + 1);
+                }
+            }
+            toResolve.AddRange(toProcess.Select(
+                component => ResolveComponent(component, template, resolved, recurseLevel)).Where(r => r != null));
             return toResolve;
         }
 
@@ -177,9 +187,11 @@ namespace Sdl.Web.DXAResolver
         /// <param name="instruction">Resolve instruction</param>
         /// <param name="context">Publish context</param>
         /// <param name="resolvedItems">Final items to resolve</param>
-        public void Resolve(IdentifiableObject item, ResolveInstruction instruction, PublishContext context, Tridion.Collections.ISet<ResolvedItem> resolvedItems)
+        public void Resolve(IdentifiableObject item, ResolveInstruction instruction, PublishContext context,
+            Tridion.Collections.ISet<ResolvedItem> resolvedItems)
         {
-            if (instruction.Purpose != ResolvePurpose.Publish && instruction.Purpose != ResolvePurpose.RePublish || _maxRecurseDepth == 0)
+            if (instruction.Purpose != ResolvePurpose.Publish && instruction.Purpose != ResolvePurpose.RePublish ||
+                _maxRecurseDepth == 0)
                 return;
             _log.Debug("DXA Custom Resolver started...");
             _log.Debug("Loading global app data:");
@@ -203,41 +215,57 @@ namespace Sdl.Web.DXAResolver
                 }
                 else
                 {
-                    _log.Debug($"Custom resolver configuration not found in global app data for '{appId}'. Using default settings.");
+                    _log.Debug(
+                        $"Custom resolver configuration not found in global app data for '{appId}'. Using default settings.");
                     _maxRecurseDepth = -1;
                 }
             }
             catch (Exception e)
             {
-                _log.Debug($"Exception occured when reading global app data for application id '{appId}'. Using default settings.");
+                _log.Debug(
+                    $"Exception occured when reading global app data for application id '{appId}'. Using default settings.");
                 _log.Debug(e.Message);
                 _maxRecurseDepth = -1;
-            }         
-          
-            var sourceItem = (RepositoryLocalObject)item;
-            var contextPublication = (Publication)sourceItem.ContextRepository;
-            var filter = new ComponentTemplatesFilter(item.Session)
-            {
-                AllowedOnPage = false,
-                BaseColumns = ListBaseColumns.IdAndTitle
-            };
-            const string dataPresentationTemplateTitle = "Generate Data Presentation";
-            var dataPresentationTemplate = contextPublication.GetComponentTemplates(filter).FirstOrDefault(
-                ct => ct.Title == dataPresentationTemplateTitle);
-            if (dataPresentationTemplate == null)
-            {
-                _log.Debug("No 'Generate Data Presentation' component template found. Skipping custom resolver.");
-                return;
             }
-            var resolved = new HashSet<IdentifiableObject>();
-            Stopwatch t = new Stopwatch();
-            t.Start();
-            foreach (var x in Resolve(item, dataPresentationTemplate, resolved, 0))
+            
+            try
             {
-                resolvedItems.Add(x);
+                var sourceItem = (RepositoryLocalObject) item;
+                var contextPublication = (Publication) sourceItem.ContextRepository;
+                var filter = new ComponentTemplatesFilter(item.Session)
+                {
+                    AllowedOnPage = false,
+                    BaseColumns = ListBaseColumns.IdAndTitle
+                };
+                const string dataPresentationTemplateTitle = "Generate Data Presentation";
+                var dataPresentationTemplate = contextPublication.GetComponentTemplates(filter).FirstOrDefault(
+                    ct => ct.Title == dataPresentationTemplateTitle);
+                if (dataPresentationTemplate == null)
+                {
+                    _log.Debug("No 'Generate Data Presentation' component template found. Skipping custom resolver.");
+                    return;
+                }
+                var resolved = new HashSet<IdentifiableObject>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                HashSet<TcmUri> alreadyResolved =new HashSet<TcmUri>();
+                foreach (var x in resolvedItems)
+                {
+                    alreadyResolved.Add(x.Item.Id);
+                }
+                foreach (var x in ResolveItem(item, dataPresentationTemplate, resolved, 0))
+                {
+                    if(!alreadyResolved.Contains(x.Item.Id)) resolvedItems.Add(x);                    
+                }
+                t.Stop();
+                _log.Debug($"DXA Custom Resolver took {t.ElapsedMilliseconds}ms to complete.");
             }
-            t.Stop();
-            _log.Debug($"DXA Custom Resolver took {t.ElapsedMilliseconds}ms to complete.");
+            catch (Exception e)
+            {
+                _log.Debug($"Exception occured: {e.Message}");
+                _log.Debug($"Stacktrace: {e.StackTrace}");                
+            }
         }
     }
 }
