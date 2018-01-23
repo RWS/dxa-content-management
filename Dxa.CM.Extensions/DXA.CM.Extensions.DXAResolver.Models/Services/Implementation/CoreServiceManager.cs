ed4b38846ffb2bcd1d93686188c4af21a74de1cf
mborysenko <mborysenko@sdl.com>
2018-01-23 17:50:42 +0200

    TSI-3000 > Cleaned up

--- Dxa.CM.Extensions/DXA.CM.Extensions.DXAResolver.Models/Services/Implementation/CoreServiceManager.cs
+++ Dxa.CM.Extensions/DXA.CM.Extensions.DXAResolver.Models/Services/Implementation/CoreServiceManager.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Claims;
 using System.ServiceModel;
 using System.Threading;
 using System.Web;
--- DxaContentManagementExtensions/DXA.CM.Extensions.CustomResolver.Editors/Views/RibbonToolbar/RibbonGroup.ascx.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tridion.Web.UI.Core.Controls;
-
-namespace DXA.CM.Extensions.CustomResolver.Editors.Views.RibbonToolbar
-{
-    [ControlResources("DXA.CM.Extensions.CustomResolver.Editors.Views.RibbonToolbar")]
-    public class RibbonGroup : Tridion.Web.UI.Controls.TridionUserControl
-    { }
-
-}
