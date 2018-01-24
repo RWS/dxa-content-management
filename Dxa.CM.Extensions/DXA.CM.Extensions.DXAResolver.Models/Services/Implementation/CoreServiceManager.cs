22d50591d2c79afb97a345806f064794495cad91
Likhan Siddiquee <likhan_s@hotmail.com>
2018-01-24 08:15:51 +0100

    changing implementation to not use static classes, as that might store wrong username context. Added saml support. leveraging dispose implemetation in the coreserviceclient.

--- Dxa.CM.Extensions/DXA.CM.Extensions.DXAResolver.Models/Services/Implementation/CoreServiceManager.cs
+++ Dxa.CM.Extensions/DXA.CM.Extensions.DXAResolver.Models/Services/Implementation/CoreServiceManager.cs
@@ -12,100 +12,46 @@ namespace DXA.CM.Extensions.DXAResolver.Models
 {
     internal class CoreServiceManager
     {
-        /// <summary>
-        /// Private constructor to force everyone to use the static <see cref="GetInstance()"/> method.
-        /// </summary>
-        /// <remarks>
-        /// This constructor should NOT be removed.
-        /// </remarks>
-        private CoreServiceManager()
-        { }
-
-        private string _userName;
-        private static Lazy<CoreServiceManager> _instance = new Lazy<CoreServiceManager>(() => new CoreServiceManager());
-
-        internal SessionAwareCoreServiceClient _coreServiceClient = null;
-
-        /// <summary>
-        /// Gets or creates an instance of <see cref="CoreServiceManager"/> that is stored in the current http context <seealso cref="HttpContext.Current"/>.
-        /// </summary>
-        /// <returns>An instance of <see cref="CoreServiceManager"/></returns>
-        public static CoreServiceManager GetInstance()
-        {
-            using (Tracer.GetTracer().StartTrace())
-            {
-                return _instance.Value;
-            }
-        }
-
-        public string UserName
+        public static string UserName
         {
             get
             {
                 using (Tracer.GetTracer().StartTrace())
                 {
-                    if (_userName == null)
+                    string userName = String.Empty;
+
+                    // Use the HttpContext if available (first choice)
+                    if (HttpContext.Current != null && HttpContext.Current.User != null &&
+                        HttpContext.Current.User.Identity != null)
                     {
-                        string userName = String.Empty;
-
-                        // Use the HttpContext if available (first choice)
-                        if (HttpContext.Current != null && HttpContext.Current.User != null &&
-                            HttpContext.Current.User.Identity != null)
-                        {
-                            userName = HttpContext.Current.User.Identity.Name;
-                        }
-                        // Use the WCF security context if available (second choice)
-                        else if (ServiceSecurityContext.Current != null)
-                        {
-                            userName = ServiceSecurityContext.Current.WindowsIdentity.Name;
-                        }
-
-                        // If neither HTTPContext or WCFSecurityContext could be used, revert back to the Thread's principal
-                        if (String.IsNullOrEmpty(userName))
-                        {
-                            userName = Thread.CurrentPrincipal.Identity.Name;
-                        }
-
-                        _userName = userName;
+                        userName = HttpContext.Current.User.Identity.Name;
+                    }
+                    // Use the WCF security context if available (second choice)
+                    else if (ServiceSecurityContext.Current != null)
+                    {
+                        userName = ServiceSecurityContext.Current.WindowsIdentity.Name;
                     }
 
-                    return _userName;
+                    // If neither HTTPContext or WCFSecurityContext could be used, revert back to the Thread's principal
+                    if (String.IsNullOrEmpty(userName))
+                    {
+                        userName = Thread.CurrentPrincipal.Identity.Name;
+                    }
+
+                    return userName;
                 }
             }
         }
 
-        public SessionAwareCoreServiceClient CoreServiceClient
-        {
-            get
-            {
-                using (Tracer.GetTracer().StartTrace())
-                {
-                    if (_coreServiceClient != null && CoreServiceClientIsFaulted())
-                    {
-                        DestroyCoreService();
-                    }
-
-                    if (_coreServiceClient != null)
-                    {
-                        return _coreServiceClient;
-                    }
-
-                    _coreServiceClient = CreateCoreService(Constants.CLIENT_ENDPOINT_NAME_81);
-
-                    return _coreServiceClient;
-                }
-            }
-        }
-
-        private bool CoreServiceClientIsFaulted()
+        public static SessionAwareCoreServiceClient GetCoreServiceClient()
         {
             using (Tracer.GetTracer().StartTrace())
             {
-                return ((ICommunicationObject)_coreServiceClient).State == CommunicationState.Faulted;
+                return CreateCoreService(Constants.CLIENT_ENDPOINT_NAME_81);
             }
         }
 
-        public SessionAwareCoreServiceClient CreateCoreService(string endpointName)
+        private static SessionAwareCoreServiceClient CreateCoreService(string endpointName)
         {
             using (Tracer.GetTracer().StartTrace(endpointName))
             {
@@ -126,33 +72,7 @@ namespace DXA.CM.Extensions.DXAResolver.Models
             }
         }
 
-        private void DestroyCoreService()
-        {
-            using (Tracer.GetTracer().StartTrace())
-            {
-                if (_coreServiceClient == null)
-                {
-                    return;
-                }
-
-                var client = _coreServiceClient as ICommunicationObject;
-                if (client != null)
-                {
-                    if (client.State == CommunicationState.Faulted)
-                    {
-                        client.Abort();
-                    }
-                    else
-                    {
-                        client.Close();
-                    }
-                }
-                ((IDisposable)_coreServiceClient).Dispose();
-                _coreServiceClient = null;
-            }
-        }
-
-        private void SetClaimsCredential(ClaimsClientCredentials clientCredentials, Uri audienceUri)
+        private static void SetClaimsCredential(ClaimsClientCredentials clientCredentials, Uri audienceUri)
         {
             using (Tracer.GetTracer().StartTrace(clientCredentials))
             {
